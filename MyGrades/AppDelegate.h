a7c68378bf9635aa470bc16428f6825c80a01d7f
Kasey Clark <kaseyclark@kaseys-macbook-pro.local>
2015-11-21 13:05:15 -0600

    Initial Commit

---
+++ MyGrades/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGrades
+//
+//  Created by Kasey Clark on 11/21/15.
+//  Copyright © 2015 StraightouttaCompiler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGrades/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGrades
+//
+//  Created by Kasey Clark on 11/21/15.
+//  Copyright © 2015 StraightouttaCompiler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
